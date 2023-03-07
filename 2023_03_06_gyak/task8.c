893281412823075e8b173e89ba56890140e2a0c3
Tivadar Cziko <72433224+cziko1@users.noreply.github.com>
2023-03-07 13:50:26 +0100

    Órai feladat
    8 feladat integerre félig kész.
    Fájlba tevés :+1:
    Hiányzik az idő mérés

---
+++ 2023_03_06_gyak/task8.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void int_arrf(int arr[], int n);
+int length_arr(int max, int min); 
+
+
+int main(){
+    int n;
+    int max, min;
+
+    printf("Maximum: "); // maximum beolvasasa
+    scanf("%d", &max);
+    
+    printf("Minimum: "); // minmum beolvasasa
+    scanf("%d", &min);
+
+    n=length_arr(max,min); // tomb hossza
+
+    printf("max and min: %d, %d\n", max, min);  // maximum & minimum
+    printf("The length of the array: %d\n", n); // tomb hossza kirasa
+
+    int arr[n]; //A tomb dekralalasa
+    int_arrf(arr,n); // tomb fugvenye
+
+    FILE *f = fopen("arr.file", "wb");
+    fwrite(arr, sizeof(char), sizeof(arr), f);    
+    fclose(f);
+
+    return 0;
+}
+int length_arr(int max, int min){   // tomb hossza fogveny ket bekert szelso ertek kozott es ez randomizalva
+    int n;
+    n=(rand()%(max-min+1)+min);
+    return n; 
+}
+void int_arrf(int arr[], int n){    //tomb fugvenye
+    int i;
+    int max, min;       // segedvaltozok
+
+    printf("Enter the interall!\n"); //random szamok intervallum
+    printf("Max: ");
+    scanf("%d", &max);
+    printf("Min: ");
+    scanf("%d", &min);
+
+    for ( i = 0; i < n; i++)
+    {
+        arr[i]=rand()%(max-min+1)+min;  // random szamok generalas
+        printf("%d. element: %d\n", i+1, arr[i]+1);
+    }
+}
+/*
+void int_arrf(int arr[], int n){    //tomb fugvenye
+    int i;
+    double max, min;       // segedvaltozok
+
+    printf("Enter the interall!\n"); //random szamok intervallum
+    printf("Max: ");
+    scanf("%lf", &max);
+    printf("Min: ");
+    scanf("%lf", &min);
+
+    for ( i = 0; i < n; i++)
+    {
+        arr[i]=rand()%(max-min+1)+min;  // random szamok generalas
+        printf("%d. element: %d\n", i+1, arr[i]+1);
+    }
+}
+ **/
+
+
